8eba73ebc8949bcfe1ee3a3c379261372dadb508
chenyang <chenyang@masols.com>
2013-12-19 19:08:53 +0800

    original loader demo
    upload some old data

---
+++ loader_with_max_thread.js
@@ -0,0 +1,430 @@
+/**************************************************************************
+ *
+ *  This file is part of the UGE(Uniform Game Engine).
+ *  Copyright (C) by SanPolo Co.Ltd. 
+ *  All rights reserved.
+ *
+ *  See http://uge.spolo.org/ for more information.
+ *
+ *  SanPolo Co.Ltd
+ *  http://uge.spolo.org/  sales@spolo.org uge-support@spolo.org
+ *
+**************************************************************************/
+
+try{
+
+require("console.js");
+require("xmlDocument.js");
+
+/// 全局对象Loader用于管理SPP所有加载问题。
+var Loader = null;
+
+(function(){
+	Loader = new Object();
+	
+	// 初始化CS层的Loader
+	var iLoader = Registry.Get('iLoader');
+	var iThreadedLoader = Registry.Get('iThreadedLoader');
+	iThreadedLoader.flags = 1;
+	
+	/// @brief 按照类型来创建临时节点。
+	var createTmpLibNode = function(node, type)
+	{
+		var tmpdoc = new xmlDocument();
+		var tmproot = tmpdoc.CreateRoot();
+		var tmplibnodes = tmproot.CreateChild(xmlDocument.NODE_ELEMENT);
+		tmplibnodes.value = "library";
+		var tmpnode = tmplibnodes.CreateChild(xmlDocument.NODE_ELEMENT);
+		if(type == "library")
+		{
+			var tmptextnode = tmpnode;
+		}
+		else
+		{
+			tmpnode.value = type;
+			var tmptextnode = tmpnode.CreateChild(xmlDocument.NODE_ELEMENT);
+		}
+		if(!xmlDocument.CopyNode(tmptextnode, node))
+		{
+			console.debug("Failed to copy node");
+			return false;
+		}
+		
+		// 如果我们在调试，那么把这些东西写入文件，方便查看。
+		if(CmdLine.GetOption("debug", 0) == "chenyang" && type == "library")
+		{
+			var filename = "/debug/" + type + Math.floor((Math.random()*10000000)+1) + ".xml";
+			console.debug(filename);
+			var file = VFS.Open(filename, VFS.WRITE);
+			if(file)
+			{
+				tmpdoc.WritetoFile(file);
+				file.Flush();
+			}
+		}
+		
+		return tmplibnodes;
+	};
+	
+	/**
+	 * @brief 根据节点名称来创建临时节点，包含在world节点中。
+	 * @details 以下演示，参数为`<sound>`节点。
+	 * <code>
+	 * <world>
+	 *   <sounds>
+	 *     <sound>
+	 *       <file>...</file>
+	 *       [...]
+	 *     </sound>
+	 *   </sounds>
+	 * </world>
+	 * <code>
+	 */
+	var createTmpWorldNode = function(node, typename)
+	{
+		var tmpdoc = new xmlDocument();
+		var tmproot = tmpdoc.CreateRoot();
+		var tmplibnodes = tmproot.CreateChild(xmlDocument.NODE_ELEMENT);
+		tmplibnodes.value = "world";
+		var tmpnode = tmplibnodes.CreateChild(xmlDocument.NODE_ELEMENT);
+		tmpnode.value = typename + "s";
+		var tmptextnode = tmpnode.CreateChild(xmlDocument.NODE_ELEMENT);
+		if(!xmlDocument.CopyNode(tmptextnode, node))
+		{
+			console.debug("Failed to copy node");
+			return false;
+		}
+		
+		// 如果我们在调试，那么把这些东西写入文件，方便查看。
+		if(CmdLine.GetOption("debug", 0) == "chenyang")
+		{
+			var filename = "/debug/" + typename + Math.floor((Math.random()*10000000)+1) + ".xml";
+			console.debug(filename);
+			var file = VFS.Open(filename, VFS.WRITE);
+			if(file)
+			{
+				tmpdoc.WritetoFile(file);
+				file.Flush();
+			}
+		}
+		
+		return tmplibnodes;
+	};
+	
+	/**
+	 * @brief 获得下一个需要加载的stage
+	 * 如果下一个stage指定的文件不存在，则继续寻找下下个stage。
+	 * 如果当前stage是数组的最后一名，则返回false
+	 */
+	var getNextStage = function(currentStageName)
+	{
+		// 使用for in结构的话，idx的类型为String，当idx+1的时候就郁闷了。
+		for(var idx = 0; idx < Loader.Stage.length; idx++)
+		{
+			if(Loader.Stage[idx].name == currentStageName)
+			{
+				// 确保不越界。
+				if((idx+1) >= Loader.Stage.length)
+				{
+					return false;
+				}
+				return Loader.Stage[idx+1];
+			}
+		}
+	};
+	
+	/**
+	 * @brief 获得指定名称的stage
+	 */
+	var getStage = function(stageName)
+	{
+		for(var idx in Loader.Stage)
+		{
+			if(Loader.Stage[idx].name == stageName)
+			{
+				return Loader.Stage[idx];
+			}
+		}
+	};
+	
+	/**
+	 * @brief 获得需要加载的所有stage的权重总和，用于计算滚动条。
+	 */
+	var getAllWeighing = function()
+	{
+		var all_weighing = 0;
+		for(var idx in Loader.Stage)
+		{
+			all_weighing += Loader.Stage[idx].weighing;
+		}
+		return all_weighing;
+	};
+	
+	/**
+	 * @brief 直接加载包含`<shaders>`的lib文件。
+	 */
+	Loader.LoadShaderLibFile = function(filename)
+	{
+		if(!VFS.Exists(filename))
+		{
+			alert("File not exist : " + filename);
+			exit(1);
+		}
+		iLoader.LoadLibrary(filename);
+	};
+	
+	/**
+	 * @brief 读取`*lib.xml`或者`materials.xml`或者`factorylib.xml`文件，加载其中所有节点
+	 * @details `materails.xml`比较特殊，包含了<textures>和<materials>两个主节点。
+	 * `factorylib.xml`比较特殊，读取的节点深度不同。
+	 */
+	Loader.LoadLibrary = function(filename, typename, onprogress, finish)
+	{
+		console.debug("begine to load " + typename + " in file " + filename);
+		
+		// 由于是并发加载，需要一个数组保存所有的返回结果。
+		var iThreadReturnList = new Array();
+		
+		var rootnode = xmlDocument.getRootNode(filename, "library");
+		
+		if(!rootnode)
+		{
+			alert("Failed to open file " + filename);
+			exit(1);
+		}
+		
+		if(typename == "meshfact")
+		{
+			var typeNodeItr = rootnode.GetChildren("library");
+		}
+		else
+		{
+			var typesNode = rootnode.GetChild(typename + "s");
+			var typeNodeItr = typesNode.GetChildren(typename);
+		}
+		
+		var finish_count = 0;
+		var finishfunc = function(){
+			finish_count++;
+			
+			console.debug("finish one thread : " + finish_count);
+			
+			// 释放内存。
+			//C3D.engine.Prepare();
+			//System.ReleaseObject(iThreadReturnList[this.index]);
+			//iThreadReturnList[this.index] = null;
+			
+			// 回调用户，进度条步进。
+			console.debug("start to update progress.");
+			onprogress(typename, {
+				total : iThreadReturnList.length,
+				loaded : finish_count
+			});
+			console.debug("end of updating progress.");
+			
+			if(this.bHasMoreNode)
+			{
+				console.debug("has more nodes to load");
+				this.loadNextNodes();
+				console.debug("finish load next node");
+			}
+			if(finish_count >= iThreadReturnList.length)
+			{
+				// 所有线程都加载完毕，让用户决定下一步进行什么。
+				console.debug("finish all thread.");
+				finish(typename);
+			}
+			
+			console.debug("finish finish_function");
+		}
+		
+		var func_loadNextNodes = function(){
+			console.debug("func_loadNextNodes");
+			var idx = iThreadReturnList.length;
+			var start = iThreadReturnList.length;
+			
+			console.debug("length of iThreadReturnList is " + idx);
+			
+			// 遍历每个`<texture>`节点，并加载之。
+			var MaxThread = 4//Global.MAX_THREAD;//xxdebug
+			while(typeNodeItr.HasNext() && MaxThread > 0)
+			{
+				console.debug("typeNodeItr.HasNext() && MaxThread > 0");
+				MaxThread--;
+				// 获取`<texture>`节点
+				var typeNode = typeNodeItr.Next();
+				
+				if(typename == "meshfact")
+				{
+					// 创建临时节点，用于加载单个
+					// typeNode = <library>/art/factories/gnd99.xml</library>
+					var tmpWorldNodes = createTmpLibNode(typeNode, "library");
+					// 加载创建出来的这个单独的节点。
+					iThreadReturnList[idx] = iThreadedLoader.LoadLibrary('/art/', tmpWorldNodes);
+				}
+				else
+				{
+					// 创建临时节点，用于加载单个
+					// tmpWorldNodes = <shader><file>/shader/ambient.xml</file></shader>
+					var tmpWorldNodes = createTmpWorldNode(typeNode, typename);
+					// 加载创建出来的这个单独的节点。
+					iThreadReturnList[idx] = iThreadedLoader.LoadMap('/art/', tmpWorldNodes, false);
+				}
+				idx++;
+			}
+			var bHasMoreNode = typeNodeItr.HasNext();
+			
+			console.debug("has node to load ? " + bHasMoreNode);
+			
+			for(var i = start; i < iThreadReturnList.length; i++)
+			{
+				console.debug("set finish function");
+				var tmpObj = {
+					index : i,
+					bHasMoreNode : bHasMoreNode,
+					loadNextNodes : func_loadNextNodes
+				};
+				iThreadReturnList[i].onfinish = spp.hitch(tmpObj,finishfunc);
+				console.debug("finish set finish function");
+			}
+		}
+		func_loadNextNodes();
+	};
+	
+	/**
+	 * @brief 解析`world.xml`文件，仅仅加载其中的`sector`节点中的内容。
+	 * @param {String} 需要解析的XML文件名称
+	 * @param {Function} 用户可以通过这个回调函数往场景中添加对象
+	 * @param {String} 当场景加载完毕之后会调用这个回调函数。
+	 */
+	Loader.LoadSector = function(filename, typename, processWorldNode, finish)
+	{
+		var rootnode = xmlDocument.getRootNode(filename, "world");
+		
+		if(!rootnode)
+		{
+			alert("Failed to open file " + filename);
+			exit(1);
+		}
+		
+		var worldChildItr = rootnode.GetChildren();
+		
+		// 在<world>中，除了<sector>节点，其他都删除。
+		{
+			while(worldChildItr.HasNext())
+			{
+				// 获取`<library>`节点
+				var node = worldChildItr.Next();
+				if(node.value != "sector")
+				{
+					console.debug("remove " + node.value);
+					rootnode.RemoveChildren(node);
+				}
+			}
+		}
+		
+		// 让用户可以定制该sector
+		processWorldNode.call(this, rootnode);
+		
+		var iThreadReturn = iThreadedLoader.LoadMap('/art/', rootnode, false);
+		iThreadReturn.onfinish = function()
+		{
+			console.debug("finish load sector");
+			finish(typename);
+		};
+	};
+	
+	/**
+	 * @brief 加载整个场景
+	 * @param {Function} param.onprocessWorldNode 回调，用户可以在加载之前往sector上添加meshobj
+	 * @param {Function} param.onloadend 回调，加载结束，执行序列交给用户。
+	 * @param {Function} param.onprogress 回调，在每次进度步进的时候调用。
+	 * @details 场景文件包括
+	 * shaderlib.xml
+	 * soundlib.xml -- 可选
+	 * materials.xml
+	 * factorylib.xml
+	 * world.xml
+	 * sequencelib.xml -- 可选
+	 * 加载过程每一个项目的相对权重。
+	 * shaders    - 2
+	 * sounds     - 10
+	 * textures   - 40
+	 * materials  - 2
+	 * meshfact   - 40
+	 * sector     - 4
+	 * sequence   - 2
+	 */
+	Loader.Load = function(param)
+	{
+		/**
+		 * @fixme 这些文件应该交给用户定制。
+		 */
+		Loader.Stage = param.stages;
+		//进度值，0～100
+		Loader.progress = 0;
+		
+		// 计算需要记载的项目
+		var all_weighing = getAllWeighing();
+		
+		// 设定进度为0
+		param.onprogress({
+			total : 100,
+			loaded : 0
+		});
+		
+		var onprogressProxy = function(typename, pe)
+		{
+			var weighing = getStage(typename).weighing;
+			var loaded = Loader.progress + (pe.loaded/pe.total)*weighing;
+			param.onprogress({
+				total : 100,
+				loaded : (loaded/all_weighing)*100
+			});
+		};
+		
+		var finishAll = function(){
+			
+			// 释放内存。
+			C3D.engine.Prepare();
+			
+			// 所有加载都结束，执行序列交给用户。
+			// 最后将进度调节成100%
+			param.onprogress({
+				total : 100,
+				loaded : 100
+			});
+		
+			console.debug("Finish loading sector defined in world.xml");
+			// 加载结束
+			param.onloadend();
+		};
+		
+		var onloadnext = function(currentStageName){
+			console.debug("Finish loading " + currentStageName);
+			// 把自己的权重加上去。
+			Loader.progress += getStage(currentStageName).weighing;
+			
+			var nextStageFilename = getNextStage(currentStageName).filename;
+			var nextStageName = getNextStage(currentStageName).name;
+			
+			// 是否还有下一个stage
+			if(!nextStageName)
+			{
+				finishAll();
+				return;
+			}
+			
+			if(nextStageName == "meshobj")
+				Loader.LoadSector(getStage("meshobj").filename, "meshobj", param.onprocessWorldNode, onloadnext);
+			else
+				Loader.LoadLibrary(nextStageFilename, nextStageName, onprogressProxy, onloadnext);
+		};
+		Loader.LoadLibrary(getStage("shader").filename, "shader", onprogressProxy, onloadnext);
+	};
+	
+})();
+
+}catch(e){
+	alert(e);
+}
---
+++ start.js
@@ -0,0 +1,260 @@
+/**************************************************************************
+ *
+ *  This file is part of the UGE(Uniform Game Engine).
+ *  Copyright (C) by SanPolo Co.Ltd. 
+ *  All rights reserved.
+ *
+ *  See http://uge.spolo.org/ for more information.
+ *
+ *  SanPolo Co.Ltd
+ *  http://uge.spolo.org/  sales@spolo.org uge-support@spolo.org
+ *
+**************************************************************************/
+
+try {
+
+	Plugin.Load("spp.behaviourlayer.jscript");
+	Plugin.Load("spp.script.gui.cegui");
+	Event.Send("application.open",true);
+
+	var count = Event.InstallHelper('3d','frame');
+	
+	// 加载系统中的 js 库
+	require("objlayout.js");	// 这里是加载 Entity 支持库
+	require("ui.js");	// 这里加载 GUI 支持库
+	// 测试用的加载库。
+	//load("/loader_with_max_thread.js"); // require("loader.js");
+	load("/loader_wo_max_thread.js"); // require("loader.js");
+	require("cursesui.js");	// 在CUI上画图用的。
+	
+	// 多线程加载开始。
+	Loader.Load({
+		stages : [{
+			name : "shader",
+			weighing : 2, // 加载时间权重
+			filename : "/art/shaderlib.xml"
+		}, /*{
+			name : "sound",
+			weighing : 10,
+			filename : "/art/soundlib.xml"
+		}, */{
+			name : "texture",
+			weighing : 40,
+			filename : "/art/materials.xml"
+		}, {
+			name : "material",
+			weighing : 2,
+			filename : "/art/materials.xml"
+		}, {
+			name : "meshfact",
+			weighing : 40,
+			filename : "/art/factorylib.xml"
+		}, {
+			name : "meshobj",
+			weighing : 2,
+			filename : "/art/world.xml"
+		}/*, {
+			name : "sequence",
+			weighing : 2,
+			filename : "/art/sequencelib.xml"
+		}*/],
+		onprocessWorldNode : onprocessWorldNode,
+		onloadend : onloadend,
+		onprogress : onprogress
+	});
+	
+	///@brief 通过这个函数可以获得进度信息
+	function onprogress(pe)
+	{
+		//以滚动条方式显示进度。
+		CursesUI.ProgressBar(pe.total, pe.loaded);
+	}
+	
+	///@brief 可以往`<world>`节点中添加新的节点。
+	///方便viewscene工具处理。
+	function onprocessWorldNode(node_world)
+	{
+	
+	}
+	
+	///@brief 加载场景完事之后，所有处理都在这个函数中展开。
+	function onloadend()
+	{
+		console.debug("on load end");
+		
+		var sectorlist = C3D.engine.sectors;
+		alert(sectorlist.FindByName("Scene"));
+		
+		//加载factory
+		Entities.LoadPropertyClassFactory('cel.pcfactory.input.standard');
+		Entities.LoadPropertyClassFactory('cel.pcfactory.camera.old');
+		Entities.LoadPropertyClassFactory('cel.pcfactory.object.mesh.collisiondetection');
+		//Entities.LoadPropertyClassFactory('cel.pcfactory.world.zonemanager');
+		Entities.LoadPropertyClassFactory('cel.pcfactory.object.mesh');
+		Entities.LoadPropertyClassFactory('cel.pcfactory.move.linear');
+		Entities.LoadPropertyClassFactory('cel.pcfactory.move.actor.standard'); 
+		Entities.LoadPropertyClassFactory('cel.pcfactory.object.light');
+		Entities.LoadPropertyClassFactory('cel.pcfactory.logic.trigger');
+
+		console.debug("finish LoadPropertyClassFactory");
+		
+		var nowrun = false;
+		var nowturn = false;
+
+		// 创建Player。
+		var player = Entities.CreateEntity();
+		player.name = 'player';
+		
+		console.debug("finish CreateEntity");
+		
+		//prop_zone = Entities.CreatePropertyClass(player,'pczonemanager');
+		prop_pm = Entities.CreatePropertyClass(player,'pcmesh');
+		prop_cam = Entities.CreatePropertyClass(player,'pcdefaultcamera');
+		prop_input = Entities.CreatePropertyClass(player,'pccommandinput');
+		prop_ment = Entities.CreatePropertyClass(player,'pclinearmovement');
+		prop_pcactor = Entities.CreatePropertyClass(player,'pcactormove');
+		prop_collision = Entities.CreatePropertyClass(player,'pccollisiondetection');
+
+		console.debug("finish CreatePropertyClass");
+		
+		//prop_zone.PerformAction('Load',['path','.'],['file','level.xml']);
+		prop_cam.PerformAction("SetCamera",['modename', 'thirdperson']);
+		prop_cam.PerformAction('SetZoneManager',['entity',player.name],['region','main'],['start','Camera']);
+		
+		// 添加移动和旋转速度 
+		prop_pcactor.PerformAction('SetSpeed',['movement',40],['running',2],['rotation',2],['jumping',3]);
+		
+		// 设定一个mesh的名称。
+		prop_pm.PerformAction('SetMesh', ['name','gnd85#1']);
+		//为消息绑定键位
+		//向前走
+		prop_input.PerformAction('Bind',['trigger','w'],['command','forward']);
+		//向后走
+		prop_input.PerformAction('Bind',['trigger','s'],['command','backward']);
+		//左转
+		prop_input.PerformAction('Bind',['trigger','a'],['command','rotateleft']);
+		//右转
+		prop_input.PerformAction('Bind',['trigger','d'],['command','rotateright']);
+		//跳跃（并未实现）
+		prop_input.PerformAction('Bind',['trigger','space'],['command','jump']);
+		//系统退出
+		prop_input.PerformAction('Bind',['trigger','q'],['command','TESTINGMYquit']);
+		//控制视角
+		prop_input.PerformAction('Bind',['trigger','e'],['command','rotateup']);
+		prop_input.PerformAction('Bind',['trigger','c'],['command','rotatedown']);
+		//切换不同的视角tab键
+		prop_input.PerformAction('Bind',['trigger','tab'],['command','changeview']);
+		//鼠标移动
+		prop_input.PerformAction('Bind',['trigger','MouseAxis0'],['command','mousemove']);
+		
+		console.debug("finish perform action.");
+		
+		//消息函数及其实现动作
+		player.behaviour = function(msgid,pc,p1,p2,p3,p4,p5){
+			//先前走
+			if(msgid.indexOf('forward1')!=-1) {
+				prop_pcactor.PerformAction('Forward', ['start', true]);
+				prop_pm.PerformAction('SetAnimation',['animation','run'],['cycle',true],['reset', false]);
+				nowrun = true;
+			}else if(msgid.indexOf('forward0')!=-1) {
+				prop_pcactor.PerformAction('Forward', ['start', false]);
+				
+				if(nowturn) {
+					prop_pm.PerformAction('SetAnimation',['animation','run'],['cycle',true],['reset', false]);
+				} else {
+					prop_pm.PerformAction('SetAnimation',['animation','stand'],['cycle',true],['reset', true]);
+				}
+				
+				nowrun = false;
+			}
+			//退出
+			if(msgid.indexOf('TESTINGMYquit0')!=-1) {
+				System.Quit();
+			}
+			if(msgid.indexOf('backward1')!=-1) {
+				prop_pcactor.PerformAction('Backward', ['start', true]);
+				prop_pm.PerformAction('SetAnimation',['animation','run'],['cycle',true],['reset', false]);
+				nowrun = true;
+			}
+			//向后走
+			if(msgid.indexOf('backward0')!=-1) {
+				prop_pcactor.PerformAction('Backward', ['start', false]);
+				if(nowturn) {
+					prop_pm.PerformAction('SetAnimation',['animation','run'],['cycle',true],['reset', false]);
+				} else {
+					prop_pm.PerformAction('SetAnimation',['animation','stand'],['cycle',true],['reset', true]);
+				}
+				
+				nowrun = false;
+			}
+			//左转
+			if(msgid.indexOf('rotateleft1')!=-1) {
+				prop_pcactor.PerformAction('RotateLeft', ['start', true]);
+
+				prop_pm.PerformAction('SetAnimation',['animation','run'],['cycle',true],['reset', false]);
+
+				nowturn = true;
+			}
+			if(msgid.indexOf('rotateleft0')!=-1) {
+				prop_pcactor.PerformAction('RotateLeft', ['start', false]);
+
+				if(nowrun) {
+					prop_pm.PerformAction('SetAnimation',['animation','run'],['cycle',true],['reset', false]);
+				} else {
+					prop_pm.PerformAction('SetAnimation',['animation','stand'],['cycle',true],['reset', true]);
+				}
+
+				nowturn = false;
+
+			}
+			//右转
+			if(msgid.indexOf('rotateright1')!=-1) {
+				prop_pcactor.PerformAction('RotateRight', ['start', true]);
+
+				prop_pm.PerformAction('SetAnimation',['animation','run'],['cycle',true],['reset', false]);
+
+				nowturn = true;
+
+			}
+			if(msgid.indexOf('rotateright0')!=-1) {
+				prop_pcactor.PerformAction('RotateRight', ['start', false]);
+				if(nowrun) {
+					prop_pm.PerformAction('SetAnimation',['animation','run'],['cycle',true],['reset', false]);
+				} else {
+					prop_pm.PerformAction('SetAnimation',['animation','stand'],['cycle',true],['reset', true]);
+				}
+				nowturn = false;
+			}
+			//向上调视角
+			if(msgid.indexOf('rotateup1')!=-1) {
+				prop_cam.SetProperty('pitchvelocity',1.0);
+			}
+			
+			if(msgid.indexOf('rotateup0')!=-1) {
+				prop_cam.SetProperty('pitchvelocity',0.0);
+
+				alert(prop_pm.GetProperty("position"));
+			}
+			//向下调视角
+			if(msgid.indexOf('rotatedown1')!=-1) {
+				prop_cam.SetProperty('pitchvelocity',-1.0);
+			}
+			
+			if(msgid.indexOf('rotatedown0')!=-1) {
+				prop_cam.SetProperty('pitchvelocity',0.0);
+			}
+			//tab改变视角
+			if(msgid.indexOf('changeview0')!=-1) {
+				prop_pcactor.PerformAction('ToggleCameraMode',[]);
+			}
+			//鼠标移动改变视角
+			if(msgid.indexOf('mousemove')!=-1) {
+				prop_cam.SetProperty('pitch', -p2[1]); //调整摄像机上下角度
+				prop_cam.SetProperty('yaw', p1[1]);
+			}
+		}
+	}
+	
+}catch(e){
+	alert('error:',e);
+}
